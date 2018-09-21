3625d19271b550febab1dc46bf8eb0307e0e0876
Kristopher <kris.bullinger@gmx.com>
2018-09-20 21:13:52 -0400

    Practice Complete

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,6 @@
-require 'bundler/setup'
-Bundler.require
-require_all 'app'
+# require 'bundler/setup'
+require_relative '../app/models/boatingtest.rb'
+require_relative '../app/models/instructor.rb'
+require_relative '../app/models/student.rb'
+# Bundler.require
+# require_all 'app'
