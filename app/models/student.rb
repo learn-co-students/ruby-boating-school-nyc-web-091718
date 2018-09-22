829285748ebb1862f1569f96e7ebae054a59bd4e
AsafDavidov <adavidov@conncoll.edu>
2018-09-22 15:22:37 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,24 @@
 class Student
+  attr_reader :first_name
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.find_student(first_name)
+    @@all.find do |student|
+      student.first_name == first_name
+    end
+  end
+
+  def initialize(first_name)
+    @first_name = first_name
+    @@all << self
+  end
+
+  def add_boating_test(name,status,instructor)
+    new_test = BoatingTest.new(self,name,status,instructor)
+  end
 
 end
