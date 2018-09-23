1703155dfd4da0c94d9bdcea48d004528b35f3e6
rogerdavidvera <rovera@vassar.edu>
2018-09-23 13:31:24 -0400

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,43 @@
 class Instructor
+  attr_accessor :name
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.get_student_test(student, test_name)
+    BoatingTest.all.find do |boating_test|
+      boating_test.student == student && boating_test.test_name == test_name
+    end
+  end
+
+  def self.get_all_student_tests(student)
+    BoatingTest.all.select do |boating_test|
+      boating_test.student == student
+    end
+  end
+
+  def self.pass_student(student, test_name)
+    binding.pry
+    self.get_student_test(student, test_name).status = "passed"
+  end
+
+  def self.fail_student(student, test_name)
+    self.get_student_test(student, test_name).status = "failed"
+  end
+
+  def self.student_grade_percentage(student)
+    all_tests = self.get_all_student_tests(student)
+    passed_tests = all_tests.select {|boating_test| boating_test.status == "passed"}
+    percentage = passed_tests.count.to_f / all_tests.count.to_f
+    puts "#{student.name} has a passing rate of #{percentage * 100}%."
+  end
+
+  def initialize(name)
+    @name = name
+    self.class.all << self
+  end
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,23 @@
 class Student
+  attr_accessor :name
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.find_student(name)
+    self.all.select {|student| student.name == name}
+  end
+
+  def initialize(name)
+    @name = name
+    self.class.all << self
+  end
+
+  def add_boating_test(test_name, status, instructor)
+    BoatingTest.new(self, test_name, status, instructor)
+  end
 
 end
