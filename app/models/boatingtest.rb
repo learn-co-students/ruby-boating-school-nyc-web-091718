d5efbc7758df3223c1cd859966c0e0761d2fd512
DDarrow123 <ddarrowphl@gmail.com>
2018-09-22 20:25:06 -0400

    Done

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -1,3 +1,23 @@
 class BoatingTest
 
+  attr_reader :student, :name, :instructor
+
+  attr_accessor :status
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+
+  def initialize(student, name, status, instructor)
+    @student = student
+    @name = name
+    @status = status
+    @instructor = instructor
+    @@all << self
+  end
+
+
 end
