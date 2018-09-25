54048efd0db8868a4565f6a772149ee591faf449
Trinacria <xcstolfax@gmail.comgit config --global user.name Christopher Stolfagit config --global user.email xcstolfax@gmail.com>
2018-09-25 11:14:36 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,27 @@
 class Student
 
+  attr_reader :first_name
+  @@all = []
+
+  def initialize(first_name)
+    @first_name = first_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_boating_test(test_name, status, instructor)
+    BoatingTest.new(self, test_name, status, instructor)
+  end
+
+  def self.find_student(first_name)
+    self.all.find do |student|
+      if self.first_name == student
+        self
+      end
+    end
+  end
+
 end
