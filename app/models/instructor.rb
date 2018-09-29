3ab22b60cf85326bacb2ad9b357804da56979b60
eric-klimowich <eklimowich@hotmail.com>
2018-09-29 12:39:42 -0400

    Squarepants

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,38 @@
 class Instructor
 
+  attr_accessor :name
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.pass_student(name, test_taken)
+    BoatingTest.all.each do |t|
+      t.test_status = "pass" if (t.student.first_name == name && t.test_name == test_taken)
+    end
+  end
+
+  def self.fail_student(name, test_taken)
+    BoatingTest.all.each do |t|
+      t.test_status = "fail" if (t.student.first_name == name && t.test_name == test_taken)
+    end
+  end
+
+  def self.student_grade_percentage(name)
+    tests_taken = BoatingTest.all.find_all do |t|
+      t.student.first_name == name
+    end
+    tests_passed = BoatingTest.all.find_all do |t|
+      t.student.first_name == name && t.test_status == "pass"
+    end
+    (tests_passed.count.to_f / tests_taken.count.to_f) * 100
+  end
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,26 @@
 class Student
 
+  attr_accessor :first_name
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.find_student(name)
+    self.all.find do |student|
+      student if student.first_name == name
+    end
+  end
+
+  def initialize(first_name)
+    @first_name = first_name
+    @@all << self
+  end
+
+  def add_boating_test(test_name, test_status, instructor)
+    BoatingTest.new(self, test_name, test_status, instructor)
+  end
+
 end
